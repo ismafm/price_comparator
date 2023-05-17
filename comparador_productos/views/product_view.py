4d1108b1db2a2c58ded91c8fd86d018a6fc994aa
ismael <ismaelbenito2000@gmail.com>
2023-05-17 20:27:00 +0200

    V5.2 Ordena tanto por precio como por ratin

--- comparador_productos/views/product_view.py
+++ comparador_productos/views/product_view.py
@@ -20,78 +20,30 @@ def search_page(request):
     return render(request, "search.html")
 
 def calc_product(request):
-
-    # recompone la lista cuando hay un valor mas barato para llevarlo a una posicion mas baja.
-    def add_new_cheapest(number, product, old_list):
-        list = [None, None, None, None, None, None, None, None, None, None]
-        var = 0
-        for l in range(len(list)):
-            if l is number:
-                list[l] = product
-                var += 1
-            else:
-                list[l] = old_list[l - var]
-
-        return list
-
-    def numeric_price_field(price):
-        #select only the numeric part in price field
-        if price is None:
-            price = "999999999"
-        real_price = price.replace("Aproximadamente","")
-        real_price = real_price.replace("EUR","")
-        real_price = real_price.replace(" ","")
-        real_price = real_price.replace("c/u","")
-        return real_price
-
-
-    # select the cheapest product in the list and put un cheapest_products list
-    def slct_cheapest_one(product_list,cheapest_products_list):
-        # range allout products and select cheapest ones
-        for index, in_product in enumerate(product_list):
-
-            # select only the numeric part of the price
-            price = in_product["price"]
-            price = numeric_price_field(price)
-            # replaces the commas with the period to make a numeric parse
-
-            # range allout the cheapest product array and insert the compared product if is cheap
-            for position_product in range(len(cheapest_products_list)):
-
-                # select one product from the list
-                product = cheapest_products_list[position_product]
-
-                if product is None or float(product.getPrice().replace(",", ".")) > float(price.replace(",", ".")):
-                    new_product = Products_repository(in_product["name"], price,
-                                                      in_product["photo"],
-                                                      in_product["link"])
-
-                    cheapest_products_list = add_new_cheapest(position_product, new_product, cheapest_products_list)
-                    break
-
-        return cheapest_products_list
-
-
-    def sort_by_price(list):
-        return sorted(list, key=itemgetter("price"))
+    def sort_list(list, dic_name, order):
+        return sorted(list, key=itemgetter(dic_name), reverse=order)
 
     #Selecciona el tipo de ordenado que se haya pedido
     def sorting_type(list, sort_type):
-        if sort_type == "Precio":
-            return sort_by_price(list)
+        if sort_type == "price":
+            return sort_list(list, "price", False)
+        elif sort_type == "rate":
+            return sort_list(list, "rate_seller", True)
+
     def ebay_products(search_product, sort_type):
         scrapydo.setup()
         scrapydo.run_spider(ebay_spider)
         cheapest_products = sorting_type(ebay_spider.product_list, sort_type)
         ebay_spider.empty_product_list()
-        return cheapest_products
+        return cheapest_products[:10]
     #product to search
     product_name = request.GET["product"].replace(" ", "+")
     # Obtain the sort type from the form
     sort_type = request.GET["sort_type"]
     # first place for the cheapest product
     list = ebay_products(product_name, sort_type)
-    request.session["lists"] = [json.dumps(list),]
+    request.session["lists"] = [list,]
+    #return HttpResponse(str(ebay_spider.contador) + "  " + str(len(list)))
     return redirect("/result/")
     #return render(request, "result.html", {"tuplita": cheapest_products})
 
--- recopilador_productos/recopilador_productos/spiders/ebay_spider.py
+++ recopilador_productos/recopilador_productos/spiders/ebay_spider.py
@@ -1,7 +1,9 @@
+import time
 
 import scrapy
 import scrapydo
-
+import scrapydo
+from scrapy.crawler import CrawlerProcess
 
 
 class ebay_spider(scrapy.Spider):
@@ -9,12 +11,13 @@ class ebay_spider(scrapy.Spider):
 
     product_list = []
     product_search = "cartera+hombre"
+    contador = 0
 
     start_urls = [
         "https://www.ebay.es/sch/i.html?_nkw=" + product_search
     ]
     def parse(self, response):
-        urls = response.xpath("//li[contains(@id,'item')]//a[@class='s-item__link']/@href")[:25].getall()
+        urls = response.xpath("//li[contains(@id,'item')]//a[@class='s-item__link']/@href")[0:30].getall()
         for url in urls:
             yield scrapy.Request(url=url, callback=self.extract_product_info)
 
@@ -29,15 +32,30 @@ class ebay_spider(scrapy.Spider):
         real_price = real_price.replace(",",".")
         return float(real_price)
 
+    def numeric_rate_field(self, rate):
+        #select only the numeric part in rate field
+        if rate is None:
+            price = "999999999"
+        real_rate = rate.replace(" ","")
+        real_rate = real_rate.replace(",",".")
+        real_rate = real_rate.replace("(","0")
+        return float(real_rate.split("%")[0])
+
     def extract_product_info(self,response):
         product = {}
         product["name"] = response.xpath("//h1[@class='x-item-title__mainTitle']//span/text()").get()
         product["price"] = self.numeric_price_field(response.xpath("//div[@class='x-bin-price__content']//span[contains(.,'EUR')]/text()").get())
         product["photo"] = response.xpath("//div[contains(@class,'active')]//img/@src").get()
         product["link"] = response.request.url
-        product["rate_seller"] = response.xpath("//li[@class='ux-seller-section__item'][last()]//span[@class='ux-textspans']/text()").get()
-
+        product["rate_seller"] = self.numeric_rate_field(response.xpath("//li[@class='ux-seller-section__item'][last()]//span[@class='ux-textspans']/text()").get())
+        product["logo"] = "img/shop_logos/logo_ebay.jpg"
+        ebay_spider.contador+=1
         ebay_spider.product_list.append(product)
     @staticmethod
     def empty_product_list():
         ebay_spider.product_list = []
+
+
+# a = CrawlerProcess()
+# a.crawl(ebay_spider)
+# a.start()
